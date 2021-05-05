a6bc26060ba209bbff9c1760514a31d17a628e05
jeffemuveyan <jemuveyan@seamfix.com>
2021-05-05 17:02:44 +0100

    Updated

--- app/src/main/java/com/seamfix/maontestapplication/ui/teams/TeamViewHolder.kt
+++ app/src/main/java/com/seamfix/maontestapplication/ui/teams/TeamViewHolder.kt
@@ -3,6 +3,7 @@ package com.seamfix.maontestapplication.ui.teams
 import android.content.Context
 import android.view.View
 import android.widget.ImageView
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.facebook.shimmer.Shimmer
@@ -12,7 +13,7 @@ import com.seamfix.maontestapplication.R
 class TeamViewHolder(var context: Context, var view: View): RecyclerView.ViewHolder(view) {
 
     var imageView: ImageView = view.findViewById(R.id.imageView)
-    var parentLayout: ConstraintLayout = view.findViewById(R.id.parentLayout)
+    var parentLayout: CardView = view.findViewById(R.id.parentLayout)
     var shimmerView: ShimmerFrameLayout =  view.findViewById(R.id.shimmer)
 
 }
